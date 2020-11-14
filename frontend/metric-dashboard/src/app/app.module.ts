7287db438da79624dc16291da7e3bfcff21ecbb2
Angelo Di Martino <angelo.dimartino@kalpa.it>
2020-11-14 22:05:23 +0100

    frontend: stompjs sample to send and receive messages

--- frontend/metric-dashboard/src/app/app.module.ts
+++ frontend/metric-dashboard/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  InjectableRxStompConfig,
+  RxStompService,
+  rxStompServiceFactory
+} from '@stomp/ng2-stompjs';
+import { myRxStompConfig } from './my-rx-stomp.config';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, MessagesComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    {
+      provide: InjectableRxStompConfig,
+      useValue: myRxStompConfig
+    },
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+      deps: [InjectableRxStompConfig]
+    }
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
